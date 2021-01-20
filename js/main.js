6cd2ee3c6f80cec52c16003f7e9e9c7bd5da6567
Marcin <mbielen1995@gmail.com>
2021-01-20 14:04:40 +0100

    About const and hoisting

--- js/main.js
+++ js/main.js
@@ -25,3 +25,22 @@ let myName = "Tomasz";
 
     console.log(letter); // return B
 })();
+
+const example = 200;
+console.log(example);
+//example = 300; //error
+
+const arrayExample = ["pierwszy"];
+console.log(arrayExample);
+//arrayExample = ["pierwszy", "drugi"]; //can't assign new objekt to const variable, error
+arrayExample[1] = "drugi"; //we can modify object by adding new element
+console.log(arrayExample);
+
+//hoisting
+console.log(varVariable); //hoisting - przenoszenie deklaracji zmiennych (bez inicjalizacji)
+                          //przez interpreter przed wykonanie kodu                        
+//console.log(letVariable); //not work for let
+//console.log(constVariable); //not work for const
+var varVariable = 1;
+//let letVariable = 1;
+//const constVariable = 1;
